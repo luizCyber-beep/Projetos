c11cdf4aebd00633c34eee2b66c8af5d41dd297f
luizCyber-beep <luizizoo564@gmail.com>
2026-05-10 17:04:47 -0300

    Refactor spacing and formatting in scripts.js

--- Projeto-Devclubs/scripts.js
+++ Projeto-Devclubs/scripts.js
@@ -1,44 +1,49 @@
-const prevButton=document.getElementById('prev')
-const nextButton=document.getElementById('next')
+const prevButton = document.getElementById('prev')
+const nextButton = document.getElementById('next')
 const items = document.querySelectorAll('.item')
 const dots = document.querySelectorAll('.dot')
 const numberIndicator = document.querySelector('.Numbers')
 const list = document.querySelector('.list')
 
 
-let active=0;
+let active = 0;
 const total = items.length;
 let timer;
 
 
-function update(direction){
+function update(direction) {
 
-     document.querySelector('.item.active').classList.remove('active')
-     document.querySelector('.dot.active').classList.remove('active')
+    document.querySelector('.item.active').classList.remove('active')
+    document.querySelector('.dot.active').classList.remove('active')
 
 
-     if(direction > 0){
+    if (direction > 0) {
         active = active + 1
-        if(active === total){
+        if (active === total) {
             active = 0
         }
-     }
-     else if(direction < 0){
+    }
+    else if (direction < 0) {
         active = active - 1
-        if(active < 0){
+        if (active < 0) {
             active = total - 1
         }
-     }
-     items[active].classList.add('active')
-     dots[active].classList.add('active')
+    }
+    items[active].classList.add('active')
+    dots[active].classList.add('active')
+
+    numberIndicator.textContent = String(active + 1).padStart(2,'0')
 }
+clearInterval(timer)
+timer = setInterval(() => {
+    update(1)
+}, 5000);
 
-
-prevButton.addEventListener('click',() => {
+prevButton.addEventListener('click', () => {
     update(-1)
 
 })
-nextButton.addEventListener('click',() => {
+nextButton.addEventListener('click', () => {
     update(1)
 
 })
